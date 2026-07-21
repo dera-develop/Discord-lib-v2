a3b1c1813484914e6fe9172930bb753712bd8a6e
dera-develop <294463521+dera-develop@users.noreply.github.com>
2026-07-22 00:57:29 +0900

    change logger path

--- sample/hello_world.py
+++ sample/hello_world.py
@@ -3,30 +3,17 @@ READMEに掲載したものと同一のものです．
 Gateway接続後，Readyイベントを受け取った際に指定されたチャンネルに
 "Hello, World!"
 と送信します．
-
-以下の場所に，該当の物を設定します．
-"your_bot_token"        : ボットトークン
-"your_os"               : OS名（GatewayのIdentifyで使用します）
-"channel_id(snowflake)" : 送信チャンネルID
-
-ログは，このプログラムと同じ階層（./log/）へ保存されます．
-ディレクトリが無い場合は，自動的に新しく作られます．
 """
-
-import os
 
 from discord_lib2.client import Bot
 from discord_lib2.logger import Logger
-
 from discord_lib2.event import GatewayEvent
 from discord_lib2.objects.resources import UserEventResources
-
-scriptPath = os.path.dirname(os.path.abspath(__file__))
 
 bot = Bot("your_bot_token", "your_os")
 
 logger = Logger()
-logger.create_default_handler(os.path.join(scriptPath, "log"))
+logger.create_default_handler("log_dir_path")
 
 class UserEvents(GatewayEvent):
   async def ready(self, resources: UserEventResources):
