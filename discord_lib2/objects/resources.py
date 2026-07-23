bb1d0d16029b7568dcdb49fcaafc234cd3dfaeac
dera-develop <294463521+dera-develop@users.noreply.github.com>
2026-07-23 15:36:17 +0900

    add logger

--- discord_lib2/objects/resources.py
+++ discord_lib2/objects/resources.py
@@ -1,11 +1,11 @@
 from discord_lib2.cache.user import DataCacheVault
-from discord_lib2.cache.onetime import OnetimeCacheVault
 from discord_lib2.objects.gateway.user_request import GatewayRequest
 from discord_lib2.objects.http_request.user_request import HttpRequest
+from discord_lib2.logger import Logger
 
 class UserEventResources:
-  def __init__(self, gateway: GatewayRequest, http_api: HttpRequest, data_cache: DataCacheVault, onetime_cache: OnetimeCacheVault) -> None:
+  def __init__(self, gateway: GatewayRequest, http_api: HttpRequest, data_cache: DataCacheVault, logger: Logger) -> None:
     self.gateway = gateway
     self.http_api = http_api
     self.cache = data_cache
-    self.cache_onetime = onetime_cache
+    self.logger = logger.get_child("GUE")
